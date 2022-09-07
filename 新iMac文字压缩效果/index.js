7a1bfec39446eed576522359de317015a1f8e9bc
abigmiu <suraelax@gmail.com>
2022-09-07 23:15:03 +0800

    1

---
+++ 新iMac文字压缩效果/index.js
@@ -0,0 +1,23 @@
+let isPinned = false
+const h2 = document.querySelector('h2')
+
+const observer = new IntersectionObserver(([e]) => {
+  console.log(e.intersectionRatio)
+  isPinned = e.intersectionRatio < 1
+  e.target.classList.toggle('pinned', isPinned)
+}, {
+  threshold: [1]
+})
+
+observer.observe(h2)
+
+document.addEventListener('scroll', (e) => {
+  if (isPinned) {
+    let html = document.documentElement
+    let height = parseInt(getComputedStyle(h2).getPropertyValue('height')) + parseInt(getComputedStyle(h2).getPropertyValue('margin-bottom'))
+    let marginTop = parseInt(getComputedStyle(h2).getPropertyValue('margin-top'))
+    let scrolled = (html.scrollTop - marginTop) / height
+
+    h2.style.setProperty('--scale', (1 - scrolled))
+  }
+})
